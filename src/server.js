0e6d4bdbf2cdfec0dd9138224d04b8431006362b
Gleider <gmdcampos@inf.ufpel.edu.br>
2019-10-24 22:34:04 -0300

    new config to able to env variables

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { config } = require('dotenv')
+const path = require('path')
 
 require('./database/index')
 
 const app = express()
+const env = process.env.NODE_ENV || 'dev'
+
+const configPath = path.join(__dirname, './config', `.env.${env}`)
+config({
+  path: configPath
+})
 app.use(express.json())
 app.use('/users', require('./routes/users'))
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Running on port 3000')
+})
