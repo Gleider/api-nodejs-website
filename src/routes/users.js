d675a1c9cefa18ced7b691170e2dd17815390748
Gleider <gmdcampos@inf.ufpel.edu.br>
2019-10-24 22:34:42 -0300

    route to test get user

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,10 @@ const UserController = require('../controllers/UserController')
 
 routes.post('/', UserController.store)
 routes.delete('/', UserController.destroy)
+routes.get('/', (req, res) => {
+  return res.status(200).json({
+    ok: 'true'
+  })
+})
 
 module.exports = routes
